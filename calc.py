5a4a96dd358176e49e5ba94c017768cb1646005d
jogeswarsarmanaredla-glitch <jogeswarnaredla6228@gmail.com>
2026-04-13 23:22:53 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,19 @@
+print("1.Add  2.Subtract  3.Multiply  4.Divide")
+choice = int(input("Enter choice: "))
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if choice == 1:
+    print("Result:", a + b)
+elif choice == 2:
+    print("Result:", a - b)
+elif choice == 3:
+    print("Result:", a * b)
+elif choice == 4:
+    if b != 0:
+        print("Result:", a / b)
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid choice")
